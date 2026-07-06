8250680cf9a2c95ef14df19dd5e47d47422849b7
Chrisp6349 <chrisparrish1@btinternet.com>
2026-07-06 20:24:02 +0100

    Add methods to load published weeks and specific week

--- api.js
+++ api.js
@@ -1,1 +1,40 @@
-class RotaAPI{static async loadRota(){const r=await fetch(CONFIG.API_URL);if(!r.ok)throw new Error("HTTP "+r.status);return await r.json();}}
+class RotaAPI {
+
+    static async loadRota() {
+
+        const r = await fetch(CONFIG.API_URL + "?action=viewer");
+
+        if (!r.ok)
+            throw new Error("HTTP " + r.status);
+
+        return await r.json();
+
+    }
+
+    static async loadPublishedWeeks() {
+
+        const r = await fetch(CONFIG.API_URL + "?action=archive");
+
+        if (!r.ok)
+            throw new Error("HTTP " + r.status);
+
+        return await r.json();
+
+    }
+
+    static async loadWeek(week) {
+
+        const r = await fetch(
+            CONFIG.API_URL +
+            "?action=publishedWeek&week=" +
+            encodeURIComponent(week)
+        );
+
+        if (!r.ok)
+            throw new Error("HTTP " + r.status);
+
+        return await r.json();
+
+    }
+
+}
